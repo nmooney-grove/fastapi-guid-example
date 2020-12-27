cd950ef7f048fc2c095201204e2fcfaeff1cbf04
Nate <nathaniel.brengle@gmail.com>
2020-12-27 14:56:24 -0800

    factor out datetime construction

--- src/fastapi_guid_example/types.py
+++ src/fastapi_guid_example/types.py
@@ -4,6 +4,15 @@ import uuid
 
 from dataclasses import dataclass
 
+def _construct_datetime(data: Any) -> datetime:
+    out = datetime.now() + timedelta(days=30)
+    if isinstance(data, int):
+        out = datetime.fromtimestamp(data)
+    if isinstance(data, datetime):
+        out = data
+    return out
+
+
 
 @dataclass
 class Entry:
@@ -16,10 +25,8 @@ class Entry:
         id_ = dict_.get("id_", uuid.uuid4()) # Generate new random guid if not provided
         user = dict_.get("user", "") # TODO not the best default for this
 
-        expires = datetime.now() + timedelta(days=30) # default
         expires_raw = dict_.get("expires")
-        if expires_raw:
-            expires = datetime.fromtimestamp(expires_raw)
+        expires = _construct_datetime(expires_raw)
 
         if not all([id_, user, expires]):
             raise ValueError() # TODO expanding this will make it more usable
